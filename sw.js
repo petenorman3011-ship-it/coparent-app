4b0c182c9e676c03c1956e5f2677bb520067c86e
Archie Norman <archienorman@Archies-Mac-mini.local>
2026-04-21 09:19:31 -0700

    Switch to demo mode for presentation

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'cokinnect-v8';
+const CACHE = 'cokinnect-v9';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
